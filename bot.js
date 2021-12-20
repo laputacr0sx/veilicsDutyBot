56bb763cc2eb7294023ba53e63db04104238bb6d
veilicsVV <laputafelix@gmail.com>
2021-12-20 16:48:28 +0800

    added hears function

--- bot.js
+++ bot.js
@@ -6,11 +6,16 @@ const dutyFile = process.env.DUTYFILE;
 
 bot.command('start', ctx => {
   bot.telegram.sendChatAction(ctx.chat.id, 'typing');
-  bot.telegram.sendMessage(ctx.chat.id, 'hello World:)', {
-    reply_markup: {
-      remove_keyboard: true,
-    },
-  });
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    `請輸入想查詢之更份
+如 129 / 525 / 881103A / 992701A 等`,
+    {
+      reply_markup: {
+        remove_keyboard: true,
+      },
+    }
+  );
 });
 
 bot.command('duty', ctx => {
@@ -40,10 +45,34 @@ bot.command('duty', ctx => {
   });
 });
 
-// bot.action('G15', ctx => {
-//   let chosen = ctx.update.callback_query.data;
-//   ctx.answerCbQuery('This is the G15 duty');
-//   ctx.reply(`You have pressed ${chosen}`);
-// });
+bot.hears(/^[135][0-5][0-9]/, ctx => {
+  console.log(ctx.match[0]);
+  bot.on(ctx.match[0], ctx => {
+    console.log(ctx);
+  });
+});
 
+bot.hears(/^8\d{5}[A-Z]?/, ctx => {
+  bot.telegram.sendChatAction(ctx.chat.id, 'typing');
+  bot.telegram.sendMessage(ctx.chat.id, 'Searching in Training ...');
+  axios.get(dutyFile).then(response => {
+    if (!response.data.duty.Training[ctx.match[0]]) {
+      bot.telegram.sendChatAction(ctx.chat.id, 'typing');
+      bot.telegram.sendMessage(ctx.chat.id, 'Sorry I cannot find your duty ');
+    } else {
+      const dutyRequired = response.data.duty.Training[ctx.match[0]];
+      ctx.deleteMessage();
+      bot.telegram.sendMessage(
+        ctx.chat.id,
+        `您查詢的${ctx.match[0]}更份資料如下
+        開工地點：${dutyRequired.bookOnLocation}
+        開工時間：${dutyRequired.bookOnTime}
+        收工時間：${dutyRequired.bookOffTime}
+        收工地點：${dutyRequired.bookOffLocation}
+        工時：${dutyRequired.duration}
+        備註：${dutyRequired.remarks}`
+      );
+    }
+  });
+});
 bot.launch();
