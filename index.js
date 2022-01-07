99f1762254e8b79169032a8d71a86202d34a8327
veilicsVV <laputafelix@gmail.com>
2022-01-07 13:28:12 +0800

    grace update

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ const moment = require('moment');
 
 const API_TOKEN =
   process.env.NODE_ENV === 'production'
-    ? process.env.BOT_TOKEN
-    : process.env.TEST_BOT_TOKEN;
+    ? process.env.BOT_TOKEN //production token
+    : process.env.TEST_BOT_TOKEN; //local dev token
 
 const bot = new Telegraf(API_TOKEN);
-const dutyFile = process.env.DUTYFILE;
 
 bot.command('start', ctx => {
   bot.telegram.sendChatAction(ctx.chat.id, 'typing');
   bot.telegram.sendMessage(
     ctx.chat.id,
-    `歡迎 ${ctx.message.from.first_name} ${ctx.message.from.last_name}
+    `歡迎 *${ctx.message.from.first_name}* 
+
 請輸入想查詢之更份
-如 129 / 525 / 881103A / 992701A 等`,
+如 129 / 525 / 881103 / 992701H 等`,
     {
+      parse_mode: 'MarkdownV2',
       reply_markup: {
         remove_keyboard: true,
       },
@@ -37,6 +38,7 @@ function sendDuty(chatId, dutyName, dutyDetail) {
     bot.telegram.sendMessage(
       chatId,
       `您查詢的 ${dutyName} 更份資料如下
+
 開工地點: ${dutyDetail.bookOnLocation}
 開工時間: ${dutyDetail.bookOnTime}
 收工時間: ${dutyDetail.bookOffTime}
@@ -44,9 +46,9 @@ function sendDuty(chatId, dutyName, dutyDetail) {
 工時: ${dutyDetail.duration}
 備註: ${dutyDetail.remarks}
 
-*點取以下字串以複製內容*
+*點取以下文字以複製內容*
 
-\`${dutyName} [${dutyDetail.bookOnLocation}]${moment(
+\`${dutyName}\n[${dutyDetail.bookOnLocation}]${moment(
         dutyDetail.bookOnTime,
         'hh:mm a'
       ).format('HH:mm')} - ${moment(dutyDetail.bookOffTime, 'HH:mm a').format(
@@ -54,31 +56,36 @@ function sendDuty(chatId, dutyName, dutyDetail) {
       )}[${dutyDetail.bookOffLocation}] ${moment(
         dutyDetail.duration,
         'hh:mm'
-      ).format('h:mm')} ${dutyDetail.remarks}\``,
+      ).format('h:mm')} <${dutyDetail.remarks}>\n\``,
       { parse_mode: 'MarkdownV2' }
     );
   }
 }
 
-bot.hears(/^([a-z]\d{2})?([135][0-5][0-9])/i, async ctx => {
-  let kb = [];
-  dutyNumber = ctx.match[0];
+bot.hears(/^([a-z]\d{2})?([135][0-5][0-9])$/i, async ctx => {
+  let kb = [[]];
+  dutyNumber = ctx.match[0].toUpperCase();
   let duty;
   bot.telegram.sendChatAction(ctx.chat.id, 'typing');
-  await axios.get(dutyFile).then(response => {
+  await axios.get(process.env.DUTYFILE).then(response => {
     duty = response.data.duty;
     Object.keys(duty).forEach(x => {
-      let row = [];
+      // let row = [];
       let button = {};
       if (x.match(/[a-z]\d{2}/gi)) {
         button.text = x;
-        row.push(button);
-        kb.push(row);
+        kb[0].push(button);
+        //only for adding rows
+        // kb.push(row);
       }
     });
     if (Object.keys(duty).includes(ctx.match[1])) {
       ctx.deleteMessage();
-      sendDuty(ctx.chat.id, ctx.match[0], duty[ctx.match[1]][ctx.match[0]]);
+      sendDuty(
+        ctx.chat.id,
+        ctx.match[0].toUpperCase(),
+        duty[ctx.match[1].toUpperCase()][ctx.match[0].toUpperCase()]
+      );
     } else {
       ctx.deleteMessage();
       bot.telegram.sendMessage(
@@ -104,26 +111,20 @@ bot.hears(/^([a-z]\d{2})?([135][0-5][0-9])/i, async ctx => {
   });
 });
 
-bot.hears(/^[89]\d{5}[A-Z]?/, async ctx => {
+bot.hears(/^[89]\d{5}[A-Z]?/i, async ctx => {
   bot.telegram.sendChatAction(ctx.chat.id, 'typing');
   const whichSpecialDuty = ctx.match[0].match(/^8.*/g) ? 'Training' : 'Special';
   bot.telegram.sendMessage(ctx.chat.id, `在${whichSpecialDuty}更份中搜尋 ...`);
 
-  await axios.get(dutyFile).then(response => {
-    const dutyRequired = response.data.duty[whichSpecialDuty][ctx.match[0]];
-
+  await axios.get(process.env.DUTYFILE).then(response => {
+    const dutyRequired =
+      response.data.duty[whichSpecialDuty][ctx.match[0].toUpperCase()];
     ctx.deleteMessage();
-
     sendDuty(ctx.chat.id, ctx.match[0], dutyRequired);
   });
 });
 
-bot.action('copy', ctx => {
-  ctx.answerCbQuery((text = '個掣未有用架喂'));
-  ctx.editMessageReplyMarkup({ reply_markup: { inline_keyboard: [[]] } });
-});
-
-bot.launch();
-
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+bot.launch();
